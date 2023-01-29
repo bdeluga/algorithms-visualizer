0f4cbe21107daac068293015753afa770053374c
Xewol <bartekdeluga0702@gmail.com>
2023-01-29 19:38:41 +0100

    added selection sort

--- src/hooks/useVisualizedAlgorithm.tsx
+++ src/hooks/useVisualizedAlgorithm.tsx
@@ -33,28 +33,26 @@ const useVisualizedAlgorithm = ({ algorithm, options }: Props) => {
     )
 
   const run = async () => {
+    const VisualizeSwap = async (i: number, j: number) => {
+      setData(prev => {
+        let newArray = [...prev]
+        let temp = newArray[i]
+        newArray[i] = newArray[j]
+        newArray[j] = temp
+        return newArray
+      })
+
+      //pause execution and visualize swap
+      await new Promise(resolve => setTimeout(resolve, options.delay_ms || 350))
+    }
+
     setIsSorting(true)
     setIterations(0)
+    let arr = [...data]
     switch (algorithm) {
       case 'bubble_sort':
         setSwaps(0)
-        const VisualizeSwap = async (i: number, j: number) => {
-          setData(prev => {
-            let newArray = [...prev]
-            let temp = newArray[i]
-            newArray[i] = newArray[j]
-            newArray[j] = temp
-            return newArray
-          })
-
-          //pause execution and visualize swap
-          await new Promise(resolve =>
-            setTimeout(resolve, options.delay_ms || 350)
-          )
-        }
-
         // copy
-        let arr = [...data]
         for (let i = 0; i < arr.length; i++) {
           for (let j = 0; j < arr.length - i - 1; j++) {
             setIterations(prev => prev + 1)
@@ -72,11 +70,47 @@ const useVisualizedAlgorithm = ({ algorithm, options }: Props) => {
         setData(arr)
         break
       case 'insertion_sort':
+        setSwaps(0)
+        for (let i = 1; i < arr.length; i++) {
+          setIterations(prev => prev + 1)
+          let key = arr[i]
+          let j = i - 1
+          while (j >= 0 && arr[j] > key) {
+            // Visualize the swap
+            await VisualizeSwap(j, j + 1)
+            setSwaps(prev => prev + 1)
+            // Perform the swap
+            arr[j + 1] = arr[j]
+            j = j - 1
+          }
+          arr[j + 1] = key
+        }
+        setData(arr)
         // code for insertion sort
         break
       case 'selection_sort':
-        // code for selection sort
+        setSwaps(0)
+        for (let i = 0; i < arr.length - 1; i++) {
+          setIterations(prev => prev + 1)
+          let minIndex = i
+          for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+              minIndex = j
+            }
+          }
+          if (minIndex !== i) {
+            // Visualize the swap
+            await VisualizeSwap(i, minIndex)
+            setSwaps(prev => prev + 1)
+            // Perform the swap
+            let temp = arr[i]
+            arr[i] = arr[minIndex]
+            arr[minIndex] = temp
+          }
+        }
+        setData(arr)
         break
+
       default:
         console.log('Invalid sort type')
     }
